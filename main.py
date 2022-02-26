a3e24ec79736d3d136edc589a37a9987e753f62b
Cristian Zuluaga <cczuluaga19@gmail.com>
2022-02-26 15:03:28 -0500

    oe

---
+++ main.py
@@ -0,0 +1,15 @@
+#comentario
+from tkinter import N
+
+
+print("que miedo la guerra")
+
+nivelAgua=int(input("digita la cantidad de agua de hidroituango: "))
+
+if(nivelAgua<200):
+    print("no tengo agua")
+elif(nivelAgua>=200 and nivelAgua<450):
+    print("todo bien, energia corriendo")
+else:
+    print("ojo con caucasia")  
+
